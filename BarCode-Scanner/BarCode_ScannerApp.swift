910a62acfd5721a26f7a57d68055346240a4664d
Leon <mrtonmoy12@gmail.com>
2024-06-03 22:31:53 +0600

    Initial Commit

---
+++ BarCode-Scanner/BarCode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCode_ScannerApp.swift
+//  BarCode-Scanner
+//
+//  Created by Mahbubur Rashid Leon on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct BarCode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
